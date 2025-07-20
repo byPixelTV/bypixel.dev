8e20b86150fbc49f782e1d3b2de2589dd65224d7
Lukas <113579672+byPixelTV@users.noreply.github.com>
2025-07-20 22:20:10 +0200

    fix: improve oauth failure error handling

--- src/components/auth/AuthFailed.tsx
+++ src/components/auth/AuthFailed.tsx
@@ -2,14 +2,21 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export default function AuthFailurePage() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error") ?? '{"message": "Authentication failed"}';
+  const errorMessage = JSON.parse(error).message === "OAuth provider failed to return email"
+    ? "OAuth provider failed to return email. Please don't remove the email scope from the OAuth provider settings."
+    : JSON.parse(error).message;
 
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen text-white">
       <h1 className="text-2xl font-bold text-red-500">Authentication Failed</h1>
-      <p>Please try again.</p>
+      <p>{errorMessage}</p>
+      <p></p>
       <div className="flex justify-end">
         <Button className="text-black hover:bg-gray-400 bg-white" onClick={() => router.push("/auth/login")}>Go back</Button>
       </div>
